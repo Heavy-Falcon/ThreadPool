c2d4275827d0adca23512543f38a91a742a2ed17
wrt <wrt5368@foxmail.com>
2021-12-16 17:31:31 +0800

    structure adjustment and function work

--- threadpool.c
+++ threadpool.c
@@ -1,42 +1,7 @@
 #include "threadpool.h"
 #include <stdio.h>
 #include <string.h>
-#include <pthread.h>
 #include <stdlib.h>
-
-// 定义任务结构体
-typedef struct Task {
-	void* (*func)(void* arg);
-	void* arg;
-}Task;
-
-// 定义线程池结构体
-struct ThreadPool {
-	// 任务队列
-	Task* taskQueue;
-	int queueCapacity;	// 队列中能容纳的最大任务数
-	int queueSize;	// 队列中任务的实际个数
-	int queueHead;	// 队头
-	int queueTail;	// 队尾
-
-	// 管理者线程
-	pthread_t managerID;
-
-	// 工作的线程
-	pthread_t* workersID;
-	int minNum;		// 最小线程数
-	int maxNum;		// 最大线程数
-	int livingNum;	// 存活的线程
-	int workingNum;	// 正在工作的线程
-	int exitNum;	// 要销毁的线程
-
-	pthread_mutex_t mutexPool;			// 锁整个线程池
-	pthread_mutex_t mutexWorkingNum;	// 锁workingNum变量
-	pthread_cond_t notEmpty;	// 任务队列不满
-	pthread_cond_t notFull;		// 任务队列不空
-
-	int shutdown;	// 要销毁线程池为1，不要销毁为0
-};
 
 ThreadPool* pool_create(int maxCapacity, int maxThreadNum, int minThreadNum) {
 	ThreadPool* pool = (ThreadPool*)malloc(sizeof(ThreadPool));	
@@ -77,7 +42,7 @@ ThreadPool* pool_create(int maxCapacity, int maxThreadNum, int minThreadNum) {
 		pool->shutdown = 0;
 
 		// 创建线程
-		pthread_create(&pool->managerID, NULL, manage, NULL);
+		pthread_create(&pool->managerID, NULL, manage, pool);
 		for (int i = 0; i < minThreadNum; i ++ )
 			pthread_create(&pool->workersID[i], NULL, work, NULL);
 		
@@ -87,5 +52,45 @@ ThreadPool* pool_create(int maxCapacity, int maxThreadNum, int minThreadNum) {
 	if (pool && pool->workersID) free(pool->workersID);
 	if (pool && pool->taskQueue) free(pool->taskQueue);
 	if (pool) free(pool);
+	return NULL;
+}
+
+void* work(void* arg) {
+	ThreadPool* pool = (ThreadPool*)arg;
+
+	while (1) {
+		pthread_mutex_lock(&pool->mutexPool);
+		// 如果任务队列为空，阻塞work线程
+		while (!pool->shutdown && pool->queueSize == 0) {
+			pthread_cond_wait(&pool->notEmpty, &pool->mutexPool);
+		}
+
+		if (pool->shutdown) {
+			pthread_mutex_unlock(&pool->mutexPool);
+			pthread_exit(NULL);
+		}
+		
+		// 从任务队列中取一个任务
+		Task task;
+		task.func = pool->taskQueue[pool->queueHead].func;
+		task.arg = pool->taskQueue[pool->queueHead].arg;
+		pool->queueHead = (pool->queueHead + 1) % pool->queueCapacity;
+		pool->queueSize -- ;
+
+		pthread_mutex_unlock(&pool->mutexPool);
+
+		pthread_mutex_lock(&pool->mutexWorkingNum);
+		pool->workingNum ++ ;
+		pthread_mutex_unlock(&pool->mutexWorkingNum);
+
+		task.func(task.arg);
+		free(task.arg);
+		task.arg = NULL;
+
+		pthread_mutex_lock(&pool->mutexWorkingNum);
+		pool->workingNum -- ;
+		pthread_mutex_unlock(&pool->mutexWorkingNum);
+
+	}
 	return NULL;
 }
--- threadpool.h
+++ threadpool.h
@@ -1,3 +1,42 @@
+#include <pthread.h>
+
 typedef struct ThreadPool ThreadPool;
 
 ThreadPool* threadPool_create(int, int, int);
+void* manage(void* arg);
+void* work(void* arg);
+
+// 定义任务结构体
+typedef struct Task {
+	void* (*func)(void* arg);
+	void* arg;
+}Task;
+
+// 定义线程池结构体
+struct ThreadPool {
+	// 任务队列
+	Task* taskQueue;
+	int queueCapacity;	// 队列中能容纳的最大任务数
+	int queueSize;	// 队列中任务的实际个数
+	int queueHead;	// 队头
+	int queueTail;	// 队尾
+
+	// 管理者线程
+	pthread_t managerID;
+
+	// 工作的线程
+	pthread_t* workersID;
+	int minNum;		// 最小线程数
+	int maxNum;		// 最大线程数
+	int livingNum;	// 存活的线程
+	int workingNum;	// 正在工作的线程
+	int exitNum;	// 要销毁的线程
+
+	pthread_mutex_t mutexPool;			// 锁整个线程池
+	pthread_mutex_t mutexWorkingNum;	// 锁workingNum变量
+	pthread_cond_t notEmpty;	// 任务队列不满
+	pthread_cond_t notFull;		// 任务队列不空
+
+	int shutdown;	// 要销毁线程池为1，不要销毁为0
+};
+
